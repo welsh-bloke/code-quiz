d090a4b732328a96781be961c2731bffcfa440e2
Mike Davies <mike@netly.co.uk>
2023-01-13 15:29:01 +0000

    added code to count down from 1 minute 30 seconds

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,12 @@
-console.log('logic.js');
+const timer = document.querySelector('#time');
+
+let time = 90;
+
+setInterval(function () {
+  let minutes = Math.floor(time/60);
+  let seconds = time % 60;
+  timer.textContent = `0${minutes}:${seconds}`;
+  time--;
+}, 1000);
+
+console.log(time);
