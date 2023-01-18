b5f948f5b72f4006757e8c6ea734895adc51ad28
Mike Davies <mike@netly.co.uk>
2023-01-18 21:50:54 +0000

    forgot to save before commiting

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,13 +2,10 @@ const highScoresContainer = document.querySelector('#highscores');
 const clearScoresButton = document.querySelector('#clear');
 let li = document.createElement('li');
 
-  if (localStorage.getItem('initials') && localStorage.getItem('score')) {
-    li.textContent = localStorage.getItem('initials') + ' - ' + localStorage.getItem('score');
-    highScoresContainer.append(li);
-  }
-
-
-
+if (localStorage.getItem('initials') && localStorage.getItem('score')) {
+  li.textContent = localStorage.getItem('initials') + ' - ' + localStorage.getItem('score');
+  highScoresContainer.append(li);
+}
 
 clearScoresButton.addEventListener('click', () => {
   localStorage.clear();
