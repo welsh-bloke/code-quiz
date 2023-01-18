aa99fd2e88fd2cdc0c38684ab60f6c0ace5f7120
Mike Davies <mike@netly.co.uk>
2023-01-18 20:34:41 +0000

    clear high scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,6 +1,12 @@
 const highScoresContainer = document.querySelector('#highscores');
+const clearScoresButton = document.querySelector('#clear');
 
 let li = document.createElement('li');
 li.textContent = localStorage.getItem('initials') + ' - ' + localStorage.getItem('score');
 
 highScoresContainer.append(li);
+
+clearScoresButton.addEventListener('click', () => {
+  localStorage.clear();
+  highScoresContainer.removeChild(li);
+})
