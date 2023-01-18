aa35ac62dfeb9a1cc9c8d2240846f769a49d7206
Mike Davies <mike@netly.co.uk>
2023-01-18 21:48:53 +0000

    only append li if there is local storage

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,10 +1,14 @@
 const highScoresContainer = document.querySelector('#highscores');
 const clearScoresButton = document.querySelector('#clear');
-
 let li = document.createElement('li');
-li.textContent = localStorage.getItem('initials') + ' - ' + localStorage.getItem('score');
 
-highScoresContainer.append(li);
+  if (localStorage.getItem('initials') && localStorage.getItem('score')) {
+    li.textContent = localStorage.getItem('initials') + ' - ' + localStorage.getItem('score');
+    highScoresContainer.append(li);
+  }
+
+
+
 
 clearScoresButton.addEventListener('click', () => {
   localStorage.clear();
